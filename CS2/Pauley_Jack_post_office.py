6f27e4e3e4a0f8e521d2eb78b689795a37859d3b
JPauley27 <jpauley27@gcds.net>
2024-09-29 21:47:23 -0400

    Add files via upload

--- CS2/Pauley_Jack_post_office.py
+++ CS2/Pauley_Jack_post_office.py
@@ -35,44 +35,63 @@ def get_cost (size,distance):
     '''
     if size == "Postcard": 
         return .20+.03*distance  
-    elif size == "large postcard": # if the size is equal to the dimensions of the large postcard
-        return .37+.03*distance # return .37 plus .03 times the distance it travels, returns the final output
-    elif size == "Envelope": # if the size is equal to the dimensions of the envlope
-        return .37+.04*distance # return .37 plus .04 times the distance it travels, returns the final output
-    elif size == "Large Envelope": # if the size is equal to the dimensions of the large envelope
-        return .60+.05*distance # return .60 plus .05 times the distance it travels, returns the final output
-    elif size == "Package": # if the size is equal to the dimensions of the package
-        return 2.95+.25*distance # return 2.95 plus .25 times the distance it travels, returns the final output
-    elif size == "Large Package": # if the size is equal to the dimensions of the large package
-        return 3.95+.35*distance # return 3.95 plus .35 times the distance it travels, returns the final output
+    elif size == "large postcard": 
+        return .37+.03*distance 
+    elif size == "Envelope": 
+        return .37+.04*distance 
+    elif size == "Large Envelope": 
+        return .60+.05*distance 
+    elif size == "Package": 
+        return 2.95+.25*distance 
+    elif size == "Large Package": 
+        return 3.95+.35*distance 
 
-def get_zone(zipcode):# defines the get zone function and the zipcode
-    if 1<= zipcode <= 6999: # if the zip code is more then one and less then 6999
-        return 1 # return zone 1
-    elif 7000<= zipcode <= 19999: # if the zipcode is more then 7000 and less that 1999
-        return 2 # returns zone 2
-    elif 20000<= zipcode <= 35999: # if the zipecode is more than 20000 and less than 35999
-        return 3 # returns zone 3
-    elif 36000<= zipcode <= 62999: # if the zipcode is more than 36000 and less than 62999
-        return 4 # returns zone 4
-    elif 63000<= zipcode <= 84999:# if the zipecode is more than 63000 and less than 84999
-        return 5# returns zone 5
-    elif 85000<= zipcode <= 99999:# if the zipcode is more than 85000 and less than 99999
-        return 6 # returns zone 6
+def get_zone(zipcode):
+    '''
+    finds the zone based on the inputed zipcodes
 
-def getType(L, H, W):   # defines getType and L,W,H
-    if (L >=3.5 and L<=4.25 and W >=0.007 and W <=0.016 and H >=3.5 and H <=6): # if length is greater than or equal to 3.5 and less than or equal to 4.25. if the width is greater than or equal to .007 and less than or equal to .016. if the height is greater than or equal to 3.5 and less than or equal to 6
-        return "Postcard" # returns the package type
-    elif (L >=4.25 and L<= 6 and W >=0.007 and W <= 0.015 and H >=6 and H <= 11.5): # if length is greater than or equal to 4.25 and less or equal to than 6. if the width is greater than or equal to .007 and less than or equal to .015. if the height is greater than or equal to 6 and less than or equal to 11.5
-        return  "Large postcard" # returns the package type
-    elif (L >=3.5 and L<=6.125 and W >= 0.016 and W <= 0.25 and H >=5 and H <= 11.5): # if length is greater than or equal to 3.5 and less than or equal to 6.125. if the width is greater than or equal to .016 and less than or equal to .25. if the height is greater than or equal to 5 and less than or equal to 11.5
-        return "Envelope" # returns the package type
-    elif (L >= 6.125 and L <= 24 and W >=0.25 and W <=0.5 and H >= 11 and H <=18): # if length is greater or equal to than 6.125 and less or equal to than 24. if the width is greater than or equal to .25 and less than or equal to .5. if the height is greater than or equal to 11 and less or equal to than 18
-        return "Large Envelope"# returns the package type 
-    elif (L+2*H+2*W<=84):# if the length plus two times height plus two times width is less than or equal to 84
-        return "Package" # return the package type
-    elif (L+2*H+2*W<=130):# if the length plus two times height plus two times width is less than or equal to 130
-        return "Large Package" # returns 
+    Args:
+        (zipcode) if the zipcode is greater than user input and less than user inputed zipcode
+    Returns:
+        returns which zone it will be put into
+    Raises:
+        (value error) if the input is a negitive or greater than 99999
+    '''
+    if 1<= zipcode <= 6999: 
+        return 1 
+    elif 7000<= zipcode <= 19999: 
+        return 2 
+    elif 20000<= zipcode <= 35999: 
+        return 3 
+    elif 36000<= zipcode <= 62999: 
+        return 4 
+    elif 63000<= zipcode <= 84999:
+        return 5
+    elif 85000<= zipcode <= 99999:
+        return 6 
+
+def getType(L, H, W):   
+    '''
+    finds the package type
+
+    Args: (getType) if the length is within certain bounds, if width is within certain bounds, if height is within certain bounds
+
+    Returns:
+        returns the package type to the user based on the user input
+    Raises:
+        (value error)if the dimmensions are negitive or greater then the largest package
+    '''
+    if (L >=3.5 and L<=4.25 and W >=0.007 and W <=0.016 and H >=3.5 and H <=6): 
+        return "Postcard" 
+    elif (L >=4.25 and L<= 6 and W >=0.007 and W <= 0.015 and H >=6 and H <= 11.5): 
+        return  "Large postcard" 
+    elif (L >=3.5 and L<=6.125 and W >= 0.016 and W <= 0.25 and H >=5 and H <= 11.5): 
+        return "Envelope" 
+    elif (L >= 6.125 and L <= 24 and W >=0.25 and W <=0.5 and H >= 11 and H <=18): 
+        return "Large Envelope"
+    elif (L+2*H+2*W<=84):
+        return "Package" 
+    elif (L+2*H+2*W<=130):
+        return "Large Package"  
     else:
-        print("doesn't meet any package requirments. Not Mailable") # prints the phrase within the qoutations
-main() # calls main function
+        print("doesn't meet any package requirments. Not Mailable") 
