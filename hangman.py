2816e13174bc21d87d88e9cb9fc28619263e57c8
JPauley27 <jpauley27@gcds.net>
2024-12-20 09:05:23 -0500

    Hangman.Pauley.py

--- hangman.py
+++ hangman.py
@@ -1,48 +1,58 @@
-import random
-hangman_pics = ['''              
-   +---+
-       |
-       |
-       |
-      ===''', '''
-   +---+
-   O   |
-       |
-       |
-      ===''', '''
-   +---+
-   O   |
-   |   |
-       |
-      ===''', '''
-   +---+
-   O   |
-  /|   |
-       |
-      ===''', '''
-   +---+
-   O   |
-  /|\  |
-       |
-      ===''', '''
-   +---+
-   O   |
-  /|\  |
-  /    |
-      ===''', '''
-   +---+
-   O   |
-  /|\  |
-  / \  |
-      ===''']
-words = ["hello","world", "python","wolf","orange","purple","yellow","red","blue","pink","green","stinky","pinky","stink","top","bottem","left","right","up","down","around","here","people","help","very","berry","apple","fruit","meat","steak","chicken","food","water","global","desktop","nothing","else","enter","leter","console","terminal","ports","problems","output","input","file","welcome","view","selection","edit","search","walk","through","source","search","explorer","test","testing","accounts","hang","man","hangman","hello","world","globe","winter","summer","fall","syrup","work","energy","formula","football","soccer","lacrosse","popper","BOP","rizz","sigma","skibidi","toilet","bridge","bus","crack","car","frat","pledge","college","donkey","unit","exam",""]
-secret = random.choice(words)
-secret_list = list(secret)
-hidden = []
-guesses = 0
+'''
+name: Jack Pauley
+description: This is a code that goes through Hangman, but the user plays against the computer-selected word.
+log: 12/19/24, 
+bugs: prints, please enter a letter, nothing else, even when it doesn't need to. 
+features: None
+sources:
 
-for character in secret_list:
-    hidden.append("_ ")
+'''
+import random #command to select a random selection from a list
+
+hangman_pics = ['''              
++---+
+    |
+    |
+    |
+    ===''', '''
++---+
+O   |
+    |
+    |
+    ===''', '''
++---+
+O   |
+|   |
+    |
+    ===''', '''
++---+
+O   |
+/|   |
+    |
+    ===''', '''
++---+
+O   |
+/|\  |
+    |
+    ===''', '''
++---+
+O   |
+/|\  |
+/    |
+    ===''', '''
++---+
+O   |
+/|\  |
+/ \  |
+    ===''']
+words = ["hello","world", "python","wolf","orange","purple","yellow","red","blue","pink","green","stinky","pinky","stink","top","bottem","left","right","up","down","around","here","people","help","very","berry","apple","fruit","meat","steak","chicken","food","water","global","desktop","nothing","else","enter","leter","console","terminal","ports","problems","output","input","file","welcome","view","selection","edit","search","walk","through","source","search","explorer","test","testing","accounts","hang","man","hangman","hello","world","globe","winter","summer","fall","syrup","work","energy","formula","football","soccer","lacrosse","popper","BOP","rizz","sigma","skibidi","toilet","bridge","bus","crack","car","frat","pledge","college","donkey","unit","exam",""]
+secret = random.choice(words) # sets the value "secret" to a random choice from the list of words
+secret_list = list(secret)# turns 
+hidden = []
+guesses = 0 # sets guesses to 0
+
+for character in secret_list: # for the number of letters in the secret list
+    hidden.append("_ ") # put the underscore
 
 print("".join(hidden))
 while "_ " in hidden and guesses < len(hangman_pics)-1:
@@ -66,12 +76,3 @@ if "_ " in hidden:
     print("You lost,the word was", secret)
 else:
     print("you won!!")
-
-def restart():
-    user_input = input("Do you want to play again?")
-    if user_input == "yes":
-        restart()
-    if user_input == "no":
-        print("Goodbye, You stink at Hangman!!")
-    
-
